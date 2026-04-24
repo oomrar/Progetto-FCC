89b2d6195595bf8663bb364d3f22f7c0f5f0cf65
oomrar <omarhoussa.oh@gmail.com>
2026-04-24 13:09:33 +0200

    Iserimento cartella

---
+++ Esercizi_prima_parte/Liste_loop_tuple.py
@@ -0,0 +1,42 @@
+#Liste semplici
+
+playlist = ["Bohemian Rhapsody", "Stairway to Heaven", "Hotel California"]
+playlist.append("Imagine")
+playlist.append("Smells Like Teen Spirit")
+playlist.insert(1, "Sweet Child O' Mine")
+playlist[0] = "Hey Jude"
+playlist.remove("Hotel California") 
+playlist.pop(1)
+
+print(len(playlist))
+print(playlist)
+
+print("-------------------------------------------------------------------------------" \
+"" \
+"" \
+"")
+
+#Esercizio con loop for e while
+#Hai una lista di prezzi (numeri) che rappresentano i prodotti nel tuo carrello. Alcuni prodotti sono difettosi (prezzo = 0) e il tuo budget è limitato.
+carrello = [["Pane", 10], ["Pasta", 20], ["Mozzarella", 0], ["Vino", 15], ["Birra", 50], ["Coca Cola", 5], ["Acqua", 12]]
+budget = 60
+totale_spesa = 0
+prodotti_acquistati = 0
+prodotti_presi = []
+for item in carrello:
+    prodotto = item[0]
+    prezzo = item[1]
+
+    if prezzo == 0:
+        print(f"Il prodotto {prodotto} è difettoso e non posso acquistarlo.")
+        continue
+
+    if totale_spesa + prezzo > budget:
+        print(f"Ho superato il budget, non posso acquistare {prodotto}.")
+        break
+
+    totale_spesa += prezzo
+    prodotti_acquistati += 1
+    prodotti_presi.append(prodotto)
+print("Ho speso in totale: €", totale_spesa, "e ho acquistato", prodotti_acquistati, "prodotti: ", prodotti_presi)
+print("Il resto è: €", budget - totale_spesa)
