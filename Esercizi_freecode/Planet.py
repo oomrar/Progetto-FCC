95ec151fa970d565d111da974ec059ec29f04b31
oomrar <omarhoussa.oh@gmail.com>
2026-06-22 17:41:54 +0200

    add

---
+++ Esercizi_freecode/Planet.py
@@ -0,0 +1,27 @@
+class Planet:
+    
+    def __init__(self, name, planet_type, star):
+        if not isinstance(name,str) or not isinstance(planet_type, str) or not isinstance(star, str):
+            raise TypeError("name, planet type, and star must be strings")
+        if not name or not planet_type or not star:
+            raise ValueError("name, planet_type, and star must be non-empty strings")
+        self.name = name
+        self.planet_type = planet_type
+        self.star = star
+
+    def orbit(self):
+        return f"{self.name} is orbiting around {self.star}..."
+    
+    def __str__(self):
+        return f"Planet: {self.name} | Type: {self.planet_type} | Star: {self.star}."
+
+planet_1 = Planet("Giove", "Gassoso", "Stella giove")
+planet_2 = Planet("Saturno", "Solido", "Stella saturno")
+planet_3 = Planet("Marte", "Solido", "Stella marte")
+
+print(planet_1)
+print(planet_2)
+print(planet_3)
+print(planet_1.orbit())
+print(planet_2.orbit())
+print(planet_3.orbit())
